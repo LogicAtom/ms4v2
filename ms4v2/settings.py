db8522cea3ae1ceab1cec3f84d5b0301513e4082
LogicAtom <anthonykoz5909@gmail.com>
2021-12-10 20:31:45 +0000

    settings and urls updated to have profiles

--- ms4v2/settings.py
+++ ms4v2/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-
+    'profiles',
 
     # Other
     'crispy_forms',
@@ -178,11 +178,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-''' # Stripe
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-'''
